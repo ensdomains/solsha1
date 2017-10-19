9b3d6bcf7837299b8246c1790455ada3ff5f2560
Nick Johnson <arachnid@notdot.net>
2017-10-19 23:11:28 +0100

    Comment out slow gas costing code

--- test/sha1.js
+++ test/sha1.js
@@ -9,9 +9,9 @@ contract('SHA1', function(accounts) {
             var instance = iSHA1.at((await SHA1.deployed()).address);
             var input = "0x" + new Buffer(v.input, 'base64').toString('hex');
             assert.equal(await instance.sha1(input), "0x" + v.sha1, input);
-            var gas = await instance.sha1.estimateGas(input);
+            /*var gas = await instance.sha1.estimateGas(input);
             totalGas += gas;
-            console.log("Cumulative gas: " + totalGas);
+            console.log("Cumulative gas: " + totalGas);*/
         });
     });
 });
